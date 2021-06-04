b386edbb1f03bd6e1252464f0ccff917e79e9539
SophiaSales <sophiabrenda10@gmail.com>
2021-06-03 22:28:57 -0300

    add config

---
+++ config/customExpress.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const consing = require('consign')
+
+module.exports = () =>{
+    const app = express()
+
+    consing()
+        .include('controllers')
+        .into(app)
+
+    return app    
+}
