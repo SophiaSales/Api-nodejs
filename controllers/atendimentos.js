d70d28d0774cedab1a097316d3d8a2c80b7ca84d
SophiaSales <sophiabrenda10@gmail.com>
2021-06-06 10:42:48 -0300

    erros em json

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -2,13 +2,20 @@ const Atendimento = require('../models/atendimentos')
 
 module.exports = app =>{
 
-    app.get('/atendimentos', (req, res) => res.send('Você esta na rota de atendimentos em GET'))
+    app.get('/atendimentos', (req, res) => {
+        Atendimento.lista(res)
+    })
+
+    app.get('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+
+        Atendimento.buscaPorId(id, res)
+    })
 
     app.post('/atendimentos', (req, res) => {
         const atendimento = req.body
 
-        Atendimento.adiciona(atendimento)
-        res.send('POST atendimento')
+        Atendimento.adiciona(atendimento, res)
     })
     
 
